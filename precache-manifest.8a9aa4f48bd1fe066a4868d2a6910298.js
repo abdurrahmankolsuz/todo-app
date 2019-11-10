edf6dd0034188c707ccd3c6eae8ca7f2d4fa18be
abdurrahmankolsuz <abdurrahmankolsuz@gmail.com>
2019-11-10 21:47:34 +0300

    Updates

---
+++ precache-manifest.8a9aa4f48bd1fe066a4868d2a6910298.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa13a801862292059d3d88e691f7dd14",
+    "url": "/jenga/index.html"
+  },
+  {
+    "revision": "ea5f82fa42f76dcd5b24",
+    "url": "/jenga/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "be3ece37279c8cc16b6d",
+    "url": "/jenga/static/css/main.8bcd9d6c.chunk.css"
+  },
+  {
+    "revision": "ea5f82fa42f76dcd5b24",
+    "url": "/jenga/static/js/2.17038d10.chunk.js"
+  },
+  {
+    "revision": "be3ece37279c8cc16b6d",
+    "url": "/jenga/static/js/main.948b256b.chunk.js"
+  },
+  {
+    "revision": "6ea6592b87a1bb3351e4",
+    "url": "/jenga/static/js/runtime-main.b99d33d3.js"
+  }
+]);
